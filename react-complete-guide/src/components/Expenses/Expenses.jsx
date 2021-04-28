84b901b2811427c932f32ceea9514f9ce77ff064
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-04-28 10:33:18 -0300

    implementação do filtro de gastos por

--- react-complete-guide/src/components/Expenses/Expenses.jsx
+++ react-complete-guide/src/components/Expenses/Expenses.jsx
@@ -1,20 +1,42 @@
+import { useState } from 'react'
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from './ExpensesFilter'
 import Card from "../UI/Card";
 import "./expenses.css";
 
 const Expenses = (props) => {
-  return (
-    <Card className="expenses">
-      {props.listOfExpenses.map((expense) => (
 
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-        
-      ))}
+  const [selectedYear, setSelectedYear] = useState('');
+  let printListOfExpenses = []
+
+  const changeSelectHandler = (selectedYear) => {
+    console.log(`[Expenses Component]: variable from ExpensesFilter ${selectedYear}`);
+    setSelectedYear(selectedYear);
+  }
+
+  if(selectedYear !== ''){ //filter returns a new array
+    printListOfExpenses = props.listOfExpenses.filter(expense => {
+      return expense.date.getFullYear().toString() === selectedYear
+    })
+  }
+  else{
+    printListOfExpenses = [...props.listOfExpenses]
+  }
+
+  return (
+
+    <Card className="expenses">
+      <ExpensesFilter defaultYear={selectedYear} onChangeSelect={changeSelectHandler} />
+      {
+        printListOfExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      }
     </Card>
     
   );
