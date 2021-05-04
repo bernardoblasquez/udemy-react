d4fa4133fddf9a3a140e78ef65cf48b40a29b4a7
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-05-04 08:02:33 -0300

    [secao 06][refact]: implementação do estado/estilo da interface adicinando classe ao invés de estilo inline

--- styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -28,15 +28,11 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label 
-          style={{color: !isValid ? 'red' : 'black'}}>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>
             Course Goal
         </label>
-        <input 
-          style={{borderColor: !isValid ? 'red' : 'black'}} 
-          type="text" 
-          onChange={goalInputChangeHandler} />
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
